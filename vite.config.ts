1fb6c61ac252c8d726a5a243c0532f2708cff591
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2025-02-25 16:37:51 +0500

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { resolve } from "path";
+import { sassPlugin } from "esbuild-sass-plugin"; // Statik import
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [svgr(), react()],
     optimizeDeps: {
-        //workaround for the problem https://github.com/vitejs/vite/issues/7719
         extensions: [".css"],
         esbuildOptions: {
             plugins: [
-                (await import("esbuild-sass-plugin")).sassPlugin({
-                    type: "style",
-                }),
+                sassPlugin({ type: "style" }) // To'g'ridan-to'g'ri chaqirish
             ],
         }
     },
     resolve: {
         alias: {
-            "@": resolve(__dirname, "src")
+            "@": resolve(__dirname, "src"),
         }
     },
     server: {
